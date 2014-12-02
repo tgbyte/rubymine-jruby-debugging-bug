1acda602f4cf3d8762baf3b382d2e3a5ce4f885d
Thilo-Alexander Ginkel <thilo@ginkel.com>
2014-12-02 14:26:29 +0100

    Screenshot

--- sample.rb
+++ sample.rb
@@ -7,4 +7,7 @@ class Test
 end
 
 x = Test.new()
+
+# Set breakpoint on next line and inspect "x" in the "Variables" view
+# Result: https://github.com/tgbyte/rubymine-jruby-debugging-bug/blob/master/screenshot.png
 puts x.inspect
